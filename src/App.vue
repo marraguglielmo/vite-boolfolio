c75a8b1a3673bd47ef77794d8ece31469bd14fe1
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-31 15:50:00 +0200

    [add] axios get, projects

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios';
+import {store} from './data/store';
+
+  export default {
+    data(){
+      return{
+        projects: [],
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(result =>{
+          this.projects = result.data
+        })
+        .catch(error =>{
+          console.log(error.message);
+        })
+      }
+    },
+
+    mounted(){
+      this.getApi();
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h3>lista progetti</h3>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.title }}</li>
+    </ul>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
