afcbc6168f6d1015b6470d08705095b3020c4a61
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-06-03 15:15:41 +0200

    [add] router in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './assets/scss/main.scss'
 import 'bootstrap/dist/css/bootstrap.css'
+import router from './router.js'
 
 import { createApp } from 'vue'
 import App from './_App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
