62339701dba84238b75e2f16b1e7fc58dd1597c2
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-06-03 18:16:05 +0200

    [add] scss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/scss/main.scss'
 import 'bootstrap/dist/css/bootstrap.css'
+import '@fontsource-variable/comfortaa'
 import {router} from './router.js'
 
 import { createApp } from 'vue'
